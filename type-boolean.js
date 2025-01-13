d252f27f7bb6aa6c48da8b37d337069d8016d962
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-01-12 21:00:35 -0300

    types boolean and strings

---
+++ type-boolean.js
@@ -0,0 +1,20 @@
+const estudante = 'Fernando' ;
+const estaAprovado = true ;
+
+
+
+
+if ( estaAprovado === true ) {
+    console.log('parabéns, boas feastas');
+
+    
+} else {
+    console.log('Reprovado, boas festas')
+}
+
+if (estudante === 'Fernando') {
+    console.log(`Olá, ${estudante}`);
+
+} else {
+    console.log ('Quem é você?')
+}
---
+++ type-strings.js
@@ -0,0 +1,13 @@
+const estudante = "Caroline";
+const docente = 'Ana';
+const cumprimento = "nosso lema é  'estudar bastante!'";
+ const citacao = `Ju diz:"nosso lema é  'estudar bastante!'"`;
+
+ console.log(cumprimento);
+ console.log(citacao);
+ 
+ console.log(`A estudante se chama ${estudante}`);
+ //crases
+
+ const senha = 'Senha segura123 ' + estudante.toUpperCase() ;
+ console.log(senha);
